f20e91d443aea63a033a586138bf2d0f6e85f891
Lizzy-gt <luiz.gaspar@aluno.senai.br>
2026-08-27 14:56:18 -0300

    sla

---
+++ src/components/ListaItem.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import {
+  Alert,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import TarefaItem from "../components/TarefaItem";
